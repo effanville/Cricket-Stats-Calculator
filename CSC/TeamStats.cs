a87c3109c4e2b0db9d79c810e3e68144b9d57735
Effanville <>
2019-09-07 17:13:57 +0100

    Adding Team Statistics.

---
+++ CSC/TeamStats.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cricket
+{
+    public class TeamStats
+    {
+        public TeamStats()
+        { }
+
+        private string fBestScore;
+        public string FBestScore
+        {
+            get { return fBestScore;
+            }
+            set { fBestScore = value; }
+        }
+    }
+}
