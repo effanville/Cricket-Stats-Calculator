53a460c641e761433a4bad1d8b34c30785ef54f7
Effanville <>
2019-09-10 21:17:22 +0100

    Created Test Structure. No Tests yet though.

---
+++ UnitTestProject1/PlayerEditTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cricket;
+using CricketStatsCalc;
+
+namespace CSCTests
+{
+    [TestClass]
+    public class PlayerEditTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Cricket_Player Matt = new Cricket_Player
+        }
+    }
+}
